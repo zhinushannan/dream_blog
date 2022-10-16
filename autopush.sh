1b80717dddf2451fa362f427fac983815d33dd02
治怒善男 <1377875184@qq.com>
2022-10-16 18:42:39 +0800

    2022-10-16_18:42:39

--- autopush.sh
+++ autopush.sh
@@ -6,8 +6,8 @@ echo 开始添加变更：git add .
 git add .
 echo;
 
-echo 开始提交变更：git commit -m '"'${now_date}'"'
-git commit -m '"'${now_date}'"'
+echo 开始提交变更：git commit -m ${now_date}
+git commit -m ${now_date}
 echo;
 
 echo 开始推送变更：git push
