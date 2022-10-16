7a83140ab36b30b78310cf94cfc5d43c6ad6a4da
治怒善男 <1377875184@qq.com>
2022-10-16 18:42:13 +0800

    "2022-10-16_18:42:13"

---
+++ autopush.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+now_date=`date +"%Y-%m-%d_%H:%M:%S"`
+
+echo 开始添加变更：git add .
+git add .
+echo;
+
+echo 开始提交变更：git commit -m '"'${now_date}'"'
+git commit -m '"'${now_date}'"'
+echo;
+
+echo 开始推送变更：git push
+git push
+echo;
